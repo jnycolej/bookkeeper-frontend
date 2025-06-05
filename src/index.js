8deab3655ac19aefaa07c9b3929c5a87e0f4d6f2
jnycolej <98425262+jnycolej@users.noreply.github.com>
2025-06-05 16:21:10 -0500

    Added user login functionality

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';    // Import your main App component
+import { AuthProvider } from './AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <React.StrictMode>
+    // <React.StrictMode>
+    //     <App />
+    // </React.StrictMode>
+    <AuthProvider>
         <App />
-    </React.StrictMode>
+    </AuthProvider>,
 );
