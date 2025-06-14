3045b6449b32299961c91abb33e8feb2926ebf8b
jnycolej <98425262+jnycolej@users.noreply.github.com>
2025-06-13 19:41:10 -0500

    Updated button styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import React, {useContext} from "react";
 //import {AuthContext} from '../AuthProvider';
 import { useAuth0} from '@auth0/auth0-react';
 import LoginButton from './LoginButton';
-import Profile from "./Profile";
+import Profile from "../pages/Profile";
 import { Navigate, useNavigate } from "react-router";
 import api from '../services/api';
 
@@ -12,18 +12,18 @@ export default function NavBar() {
         const { logout } = useAuth0()
 
         return (
-            <button onClick={() => logout({ logoutParams: {returnTo: window.location.origin}})}>
+            <button className="btn btn-outline-secondary" onClick={() => logout({ logoutParams: {returnTo: window.location.origin}})}>
                 Log Out
             </button>
         );
     };
     const navigate = useNavigate();
     return (
-        <nav>
+        <nav className="">
             <button onClick={() => navigate('/home')} className="btn btn-primary">Home</button>
             <LoginButton/>
             <LogoutButton />
-            <button onClick={() => navigate('/profile')}>Profile</button>
+            <button className="btn btn-primary" onClick={() => navigate('/profile')}>Profile</button>
         </nav>
     );
 }
