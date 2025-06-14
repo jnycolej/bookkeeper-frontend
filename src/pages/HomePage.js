ad41ec422b883c13e91249d269fad3d025beff7b
jnycolej <98425262+jnycolej@users.noreply.github.com>
2025-06-14 02:01:59 -0500

    Updated main page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,28 @@
 // Main homepage
-
+import React, {useState, useEffect} from "react";
 import BookList from "../components/BookList";
 import BookForm from "../components/BookForm";
-import Profile from "../components/Profile";
+import Profile from "./Profile";
 import { withAuthenticationRequired } from '@auth0/auth0-react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JS (optional)
 
 const HomePage = () => {
+    //     useEffect(() => {
+    //     if (!isAuthenticated) return;
+    //     const fetchProfile = async () => {
+    //         try {
+    //             const token = await getAccessTokenSilently();
+    //             const res = await api.get('/users/me', {
+    //                 headers: {Authorization: `Bearer ${token}`}
+    //             });
+    //             setProfile(res.data);
+    //         } catch (e) {
+    //             console.error('Failed to fetch profile:', e);
+    //         }
+    //     };
+    //     fetchProfile();
+    // }, [isAuthenticated, getAccessTokenSilently]);
     return (
         <div>
             <BookList />
