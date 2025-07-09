d102cdca36ce6383b559e8876c2b8a5b18386db0
jnycolej <98425262+jnycolej@users.noreply.github.com>
2025-07-09 13:03:40 -0500

    adjusted the page to add a simple book list and add the feature banner

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,42 @@
 // Main homepage
 import React, {useState, useEffect} from "react";
 import BookList from "../components/BookList";
+import NavBar from "../components/NavBar";
+import FeatureBanner from '../components/FeatureBanner';
 import BookForm from "../components/BookForm";
 import Profile from "./Profile";
-import { withAuthenticationRequired } from '@auth0/auth0-react';
+import { withAuthenticationRequired, useAuth0 } from '@auth0/auth0-react';
+import { getBooks } from "../services/bookService";
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JS (optional)
 import '../assets/bookkeeper.css';
 
 const HomePage = () => {
+    const { getAccessTokenSilently} = useAuth0();
+
+    const [books, setBooks] = useState([]);
+    //Get all books
+   useEffect(() => {
+     const fetchBooks = async () => {
+       const token = await getAccessTokenSilently();
+       const data  = await getBooks(token);
+       setBooks(data);
+     };
+     fetchBooks();
+   }, [getAccessTokenSilently]);
     return (
         <body>
-            <BookList />
+            <NavBar />
+            <div>
+                <FeatureBanner />
+            </div>
+            <div className="list-body">
+            <BookList
+                books={books}
+                onRowClick={id => navigate(`/books/${id}`)}
+            />                
+            </div>
+
         </body>
     )
 }
