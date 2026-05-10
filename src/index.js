e159b5edbef99c43f82b05ad7a7d6aad1196cebb
Jennifer Joseph <jennifer.n.joseph@gmail.com>
2026-05-09 20:27:07 -0500

    Update Auth0 audience config

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App';    // Import your main App component
 
 import {Auth0Provider} from '@auth0/auth0-react';
-import { redirect } from 'react-router';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
